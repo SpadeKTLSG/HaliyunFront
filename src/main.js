66367033264494c5320ac24e7a52e44da129d8c6
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2025-02-19 16:21:09 +0800

    initial

--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
-import { createApp } from 'vue'
-import './style.css'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
+import ElementPlus from 'element-plus'
+import locale from 'element-plus/lib/locale/lang/zh-cn'
 import App from './App.vue'
+import moment from 'moment'
 
-createApp(App).mount('#app')
+
+const pinia = createPinia() // 用于 defineStore 页面存储
+
+const momentLocaleConfig = {
+    longDateFormat: {
+        LT: 'HH:mm',
+        LTS: 'HH:mm:ss',
+        L: 'YYYY-MM-DD',
+        LL: 'YYYY年MM月DD日',
+        LLL: 'YYYY年MM月DD日 HH:mm',
+        LLLL: 'YYYY年MM月DD日 dddd HH:mm'
+    }
+}
+moment.locale('zh-cn', momentLocaleConfig)
+
+
+const app = createApp(App)
+app.use(pinia)
+app.use(ElementPlus, {locale})
+app.mount('#app')
