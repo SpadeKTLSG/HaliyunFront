5eee0495661bff009b077e13fd750835ce9004d1
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2025-04-03 11:30:22 +0800

    群组管理 2  - 群组小院 弹框 详情完成, 开始对应开发群组小院.

--- src/components/Cluster/clusters/cluster_yard.vue
+++ src/components/Cluster/clusters/cluster_yard.vue
@@ -1,41 +1,144 @@
 <template>
 
   <!--主体-->
-  <div class="userlevel">
+  <div class="clusterhall">
 
-    <!-- 上部区域: 等级金字塔-->
-    <div class="userlevel_upper">
-      <div class="level" v-for="level in 8" :key="level">
-        <el-button @click="showLevelinfo( 8-level)"
-                   :class="'level' + (level - 1)"
-                   :round="true"
-                   :size="'large'">
-          {{ 8 - level }}级
-        </el-button>
-      </div>
+    <!-- 上部区域: 操作按钮集合-->
+    <div class="clusterhall_upper">
+      <el-col :span="10">
+
+        <!--刷新-获取数据-->
+        <el-button @click="searchClusterCollect()" class="clusterhall_op-buttons">刷新</el-button>
+
+      </el-col>
     </div>
 
 
     <el-divider></el-divider>
 
-    <!-- 下部区域: 等级描述-->
-    <div class="userlevel_lower">
-      <el-table v-if="computedLevelData" :data="[computedLevelData]" style="width: 100%">
-        <el-table-column prop="name" label="等级名称"></el-table-column>
-        <el-table-column prop="cut" label="折扣优惠小数倍率"></el-table-column>
-        <el-table-column prop="grow" label="到下一级需要成长值"></el-table-column>
-        <el-table-column prop="desc" label="等级描述"></el-table-column>
-        <el-table-column prop="remark" label="等级备注"></el-table-column>
+    <!-- 下部区域: 群组展示-->
+    <div class="clusterhall_lower">
+
+      <el-table :data="pageData.records" style="width: 90%">
+        <el-table-column prop="pic" label="封面"></el-table-column>
+        <el-table-column prop="name" label="群组名称"></el-table-column>
+        <el-table-column prop="nickname" label="群组别名"></el-table-column>
+        <el-table-column prop="popVolume" label="容量"></el-table-column>
+        <el-table-column prop="createTime" label="创建时间"></el-table-column>
+        <el-table-column prop="updateTime" label="更新时间"></el-table-column>
+
+
+        <el-table-column label="操作">
+          <template #default="scope">
+            <el-button @click="viewDetails(scope.row)"
+                       type="primary"
+                       style="width: 40px"
+            >
+              详情
+            </el-button>
+            <el-button @click="joinGroup(scope.row)"
+                       type="success"
+                       style="width: 40px"
+            >
+              加入
+            </el-button>
+          </template>
+        </el-table-column>
+
       </el-table>
-      <el-text v-else> 等级数据加载中...</el-text>
+
+      <!--之后统一用这种双向绑定的-->
+      <el-pagination
+          layout="total, prev, pager, next"
+          v-model:current-page="pageData.current"
+          :page-size="pageData.size"
+          :total="pageData.total"
+          style="margin-top: 20px;"
+      />
     </div>
 
-    <div>
-      <el-text class="simple_text_red">等级信息</el-text>
-    </div>
-    <el-text class="simple_text_red"> 您的等级为 {{ stackLevelData.value }}</el-text>
-    <el-text class="simple_text_red"> 选中的等级为 {{ computedLevelData.floor }}</el-text>
+    <!-- 详情信息对话框 (查全部群组信息)  -->
+    <el-dialog
+        v-model="detailsDialogVisible"
+        title="群组详情"
+    >
 
+      <div>
+        <img alt="Image" src="@/../static/img/ClusterShowPic.png" style="width:10%; height: auto; padding-left: 80px">
+      </div>
+
+      <el-text class="simple_text_red">群组基础信息</el-text>
+
+      <el-descriptions :column="2" border size="small">
+        <el-descriptions-item label="群组名称">{{ selectedGroup.name }}</el-descriptions-item>
+        <el-descriptions-item label="群组昵称">{{ selectedGroup.nickname }}</el-descriptions-item>
+        <el-descriptions-item label="容量">{{ selectedGroup.popVolume }}</el-descriptions-item>
+        <el-descriptions-item label="使用空间大小">{{ selectedGroup.usedSpace }}</el-descriptions-item>
+        <el-descriptions-item label="总空间大小">{{ selectedGroup.totalSpace }}</el-descriptions-item>
+        <el-descriptions-item label="创建人账号">{{ selectedGroup.userAccount }}</el-descriptions-item>
+        <el-descriptions-item label="管理创建">{{ (selectedGroup.userIsAdmin) === 0 ? '是' : '否' }}</el-descriptions-item>
+        <el-descriptions-item label="允许加入">{{ (selectedGroup.allowInvite) === 0 ? '是' : '否' }}</el-descriptions-item>
+        <el-descriptions-item label="创建时间">{{ selectedGroup.createTime }}</el-descriptions-item>
+        <el-descriptions-item label="修改时间">{{ selectedGroup.updateTime }}</el-descriptions-item>
+      </el-descriptions>
+
+
+      <el-text class="simple_text_red">相册</el-text>
+      <div>
+        <img alt="Image" src="@/../static/img/ClusterShowPic.png" style="width:10%; height: auto; padding-left: 80px">
+        <img alt="Image" src="@/../static/img/ClusterShowPic.png" style="width:10%; height: auto; padding-left: 80px">
+        <img alt="Image" src="@/../static/img/ClusterShowPic.png" style="width:10%; height: auto; padding-left: 80px">
+        <img alt="Image" src="@/../static/img/ClusterShowPic.png" style="width:10%; height: auto; padding-left: 80px">
+      </div>
+
+
+      <el-text class="simple_text_red">公告</el-text>
+      <el-descriptions :column="1" border size="small">
+        <el-descriptions-item label="公告名称">{{ selectedGroup.noticeName }}</el-descriptions-item>
+        <el-descriptions-item label="公告内容">{{ selectedGroup.noticeContent }}</el-descriptions-item>
+      </el-descriptions>
+
+
+      <el-text class="simple_text_red">货币情况</el-text>
+      <div>
+        <img alt="Image" src="@/../static/img/CurrencyShowPic.png" style="width:10%; height: auto; padding-left: 80px">
+      </div>
+      <el-descriptions :column="2" border size="small">
+        <el-descriptions-item label="货币名称">{{ selectedGroup.currencyName }}</el-descriptions-item>
+        <el-descriptions-item label="货币储量">{{ selectedGroup.currencyStock }}</el-descriptions-item>
+        <el-descriptions-item label="货币硬币转化率">{{ selectedGroup.currencyExchangeRate }}</el-descriptions-item>
+        <el-descriptions-item label="硬币储量">{{ selectedGroup.coinStock }}</el-descriptions-item>
+      </el-descriptions>
+
+      <el-text class="simple_text_red">群组评论</el-text>
+      <el-descriptions :column="2" border size="small">
+        <el-descriptions-item label="大热评">{{ selectedGroup.content[0] }}</el-descriptions-item>
+        <el-descriptions-item label="热评">{{ selectedGroup.content[1] }}</el-descriptions-item>
+        <el-descriptions-item label="评">{{ selectedGroup.content[2] }}</el-descriptions-item>
+      </el-descriptions>
+
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="detailsDialogVisible = false"
+                   class="clusterhall_close-button"
+        >关闭</el-button>
+      </span>
+
+    </el-dialog>
+
+    <!-- 加入群组对话框 -->
+    <el-dialog
+        v-model="joinDialogVisible"
+        title="加入群组"
+    >
+
+      <p>确认加入群组: {{ selectedGroup.name }}?</p>
+
+
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="joinDialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="confirmJoinGroup">确认</el-button>
+      </span>
+    </el-dialog>
 
   </div>
 </template>
@@ -43,7 +146,6 @@
 <script setup>
 import * as Maven from '@/components/common/maven.js'
 import {inject} from "vue";
-import {UserContext} from "@/components/common/user.js";
 
 let ElButton, ElCard, ElCascader, ElCol, ElConfigProvider, ElDialog, ElDropdown, ElDropdownItem, ElDropdownMenu, ElForm, ElFormItem, ElInput, ElInputNumber, ElMenu, ElMenuItem,
     ElMenuItemGroup, ElPopover, ElRadio, ElRadioGroup, ElRow, ElScrollbar, ElSubMenu, ElTable, ElTableColumn, ElTag, ElText, ElTooltip, ElMessage, ref, watch, reactive, onMounted,
@@ -66,142 +168,227 @@ const emit = defineEmits(['close']);
 const currentPage = inject('currentPage');
 
 
-//? 用户数据展示表单 (对应子页签分区数据) 复用
-const levelData = ref({
-  cut: 1,
-  desc: "就是个寄吧",
-  floor: 0,
-  grow: 50,
-  id: 1894667686932631600n,
-  name: "青铜",
-  remark: "新人入职转生"
+// 群组分页数据展示表单
+const pageData = reactive({
+  current: 1,
+  size: 10,
+  total: 0,
+  records: []
 });
 
-// 用户的等级 floor:
-const userLevel = ref(0);
-const level = ref(0);
+
+// 选中的群组 (id查)
+const selectedGroup = ref({
+
+  // Cluster
+  id: 0n,
+  name: '',
+  nickname: '',
+  pic: '',
+  popVolume: 0,
+
+
+  // ClusterDetail
+  shareLink: '',
+  album: '',
+  usedSpace: 0n,
+  totalSpace: 0n,
+  createTime: '',
+  updateTime: '',
+
+
+  // ClusterFunc
+  allowInvite: 0,
+  currencyStock: 0n,
+  coinStock: 0n,
+
+
+  // Creator
+  userAccount: '',
+  userIsAdmin: 0,
+
+
+  // Notice
+  noticeName: '',
+  noticeContent: '',
+
+
+  //Currency
+  currencyName: '',
+  currencyExchangeRate: '',
+  currencyPic: '',
+
+  //Remarks
+  content: [],
+
+});
+
+// 对话框可见性
+const detailsDialogVisible = ref(false);
+const joinDialogVisible = ref(false);
+
+
+// 查看详情
+const viewDetails = (group) => {
+  getOneCluster(group.id);
+  detailsDialogVisible.value = true;
+};
+
+// 加入群组
+const joinGroup = (group) => {
+  Object.assign(selectedGroup, group);
+  joinDialogVisible.value = true;
+};
+
 
 //! 查
 
 
-onMounted(() => {
-  getUserLevel();
+onMounted(async () => {
+  await getAllClusterPage(pageData.current, pageData.size);
 });
 
 
-/**
- * 获取用户等级数据
- */
-const getUserLevel = () => {
+// 查询按钮点击事件
+const searchClusterCollect = () => {
+  getAllClusterPage(pageData.current, pageData.size);
+};
 
-  http({
-    url: http.adornUrl('Guest/users/userslevel/floor'),
-    method: 'get',
-    params: http.adornParams({
-      id: UserContext.getUserId()
-    })
-  }).then(({data}) => {
-    userLevel.value = data;
-    showLevelinfo(userLevel.value);
-  }).catch((error) => {
+
+/**
+ * 分页获取群组数据
+ */
+const getAllClusterPage = async (current, size) => {
+  try {
+    const {data} = await http({
+      url: http.adornUrl('Cluster/clusters/hall/all'),
+      method: 'get',
+      params: {
+        current,
+        size
+      }
+    });
+    pageData.current = data.current;
+    pageData.size = data.size;
+    pageData.total = data.total;
+    pageData.records = data.records.map(record => {
+      return {
+        ...record,
+        id: BigInt(record.id) // 将 id 转换为 BigInt 类型
+      };
+    });
+
+
+    if (pageData.total === 0) {
+      ElMessage({
+        message: '暂无数据',
+        type: 'warning',
+        duration: 1000
+      });
+    }
+
+  } catch (error) {
     ElMessage({
-      message: '获取用户等级数据失败: ' + error.message,
+      message: '获取群组大厅数据失败: ' + error.message,
       type: 'error',
       duration: 1000
     });
-  });
-
+  }
 
 };
 
 
 /**
- * 展示等级信息
+ * id查单个群组详情
  */
-const showLevelinfo = (level) => {
+const getOneCluster = (id) => {
   http({
-    url: http.adornUrl('Guest/levels/levelinfo/floor'),
+    url: http.adornUrl('Cluster/clusters/hall/one'),
     method: 'get',
-    params: http.adornParams({
-      floor: level
-    })
+    params: {
+      id
+      //id: id.toString() // 将 BigInt 转换为字符串传递给后端
+    }
   }).then(({data}) => {
-    levelData.value = data;
+    selectedGroup.value = data
   }).catch((error) => {
     ElMessage({
-      message: '获取楼层数据失败: ' + error.message,
+      message: '获取群组详情失败: ' + error.message,
       type: 'error',
       duration: 1000
     });
   });
-};
+}
 
-// 监听用户等级变化
-const computedLevelData = computed(() => levelData.value);
-const stackLevelData = computed(() => userLevel);
+
+// 确认加入群组
+const confirmJoinGroup = async () => {
+  try {
+    await http({
+      url: http.adornUrl(`Cluster/clusters/join`),
+      method: 'post'
+    });
+    ElMessage({
+      message: '加入群组成功',
+      type: 'success',
+      duration: 1000
+    });
+    joinDialogVisible.value = false;
+    await getAllClusterPage();
+  } catch (error) {
+    ElMessage({
+      message: '加入群组失败: ' + error.message,
+      type: 'error',
+      duration: 1000
+    });
+  }
+}
+
 
 </script>
 
 
 <style lang="scss" scoped>
 
-.userlevel {
+.clusterhall {
 
-  .userlevel_upper {
+  .clusterhall_upper {
+    display: flex;
+    flex-direction: column;
+
+    margin-top: -100px;
+    margin-bottom: 5px;
+
+
+    .clusterhall_op-buttons {
+      width: 10% !important;
+    }
+
+  }
+
+  .clusterhall_lower {
     display: flex;
     flex-direction: column;
     align-items: center;
     margin-top: 10px;
     margin-bottom: 20px;
-
-    .level {
-      display: flex;
-      justify-content: center;
-      margin-bottom: 10px;
-
-      el-button {
-        width: 100% !important;
-      }
-    }
-
-    .level0 {
-      width: 30px !important;
-    }
-
-    .level1 {
-      width: 50px !important;
-    }
-
-    .level2 {
-      width: 70px !important;
-    }
-
-    .level3 {
-      width: 90px !important;
-    }
-
-    .level4 {
-      width: 110px !important;
-    }
-
-    .level5 {
-      width: 130px !important;
-    }
-
-    .level6 {
-      width: 150px !important;
-    }
-
-    .level7 {
-      width: 170px !important;
-    }
   }
 }
 
 .simple_text_red {
+  margin: 5px;
   padding: 25px;
   font-size: 20px;
   font-weight: bold;
   color: #ee0b1a;
+}
+
+
+.clusterhall_close-button {
+  display: flex;
+  width: 15%;
+  justify-content: center;
+  margin-left: 30%;
+  margin-top: 20px;
 }
 </style>
