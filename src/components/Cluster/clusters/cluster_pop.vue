e470c1e389af0e906afbf5337c0ba4afd6992408
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2025-04-05 18:09:14 +0800

    群组信息 2 群组人员 (单一群组) 规范一下JS的顺序

--- src/components/Cluster/clusters/cluster_pop.vue
+++ src/components/Cluster/clusters/cluster_pop.vue
@@ -87,92 +87,11 @@
       />
     </div>
 
-    <!-- 人员信息弹框  -->
-    <el-dialog
-        v-model="detailsDialogVisible"
-        title="群组详情"
-    >
-
-      <div>
-        <img alt="Image" src="@/../static/img/ClusterShowPic.png" style="width:10%; height: auto; padding-left: 80px">
-      </div>
-
-      <el-text class="simple_text_red">人员基础信息</el-text>
-
-      <el-descriptions :column="2" border size="small">
-        <el-descriptions-item label="群组名称">{{ selectedGroup.name }}</el-descriptions-item>
-        <el-descriptions-item label="群组昵称">{{ selectedGroup.nickname }}</el-descriptions-item>
-        <el-descriptions-item label="容量">{{ selectedGroup.popVolume }}</el-descriptions-item>
-        <el-descriptions-item label="使用空间大小">{{ selectedGroup.usedSpace }}</el-descriptions-item>
-        <el-descriptions-item label="总空间大小">{{ selectedGroup.totalSpace }}</el-descriptions-item>
-        <el-descriptions-item label="创建人账号">{{ selectedGroup.userAccount }}</el-descriptions-item>
-        <el-descriptions-item label="管理创建">{{ (selectedGroup.userIsAdmin) === 0 ? '是' : '否' }}</el-descriptions-item>
-        <el-descriptions-item label="允许加入">{{ (selectedGroup.allowInvite) === 0 ? '是' : '否' }}</el-descriptions-item>
-        <el-descriptions-item label="创建时间">{{ selectedGroup.createTime }}</el-descriptions-item>
-        <el-descriptions-item label="修改时间">{{ selectedGroup.updateTime }}</el-descriptions-item>
-      </el-descriptions>
-
-
-      <el-text class="simple_text_red">相册</el-text>
-      <div>
-        <img alt="Image" src="@/../static/img/ClusterShowPic.png" style="width:10%; height: auto; padding-left: 80px">
-        <img alt="Image" src="@/../static/img/ClusterShowPic.png" style="width:10%; height: auto; padding-left: 80px">
-        <img alt="Image" src="@/../static/img/ClusterShowPic.png" style="width:10%; height: auto; padding-left: 80px">
-        <img alt="Image" src="@/../static/img/ClusterShowPic.png" style="width:10%; height: auto; padding-left: 80px">
-      </div>
-
-
-      <el-text class="simple_text_red">公告</el-text>
-      <el-descriptions :column="1" border size="small">
-        <el-descriptions-item label="公告名称">{{ selectedGroup.noticeName }}</el-descriptions-item>
-        <el-descriptions-item label="公告内容">{{ selectedGroup.noticeContent }}</el-descriptions-item>
-      </el-descriptions>
-
-
-      <el-text class="simple_text_red">货币情况</el-text>
-      <div>
-        <img alt="Image" src="@/../static/img/CurrencyShowPic.png" style="width:10%; height: auto; padding-left: 80px">
-      </div>
-      <el-descriptions :column="2" border size="small">
-        <el-descriptions-item label="货币名称">{{ selectedGroup.currencyName }}</el-descriptions-item>
-        <el-descriptions-item label="货币储量">{{ selectedGroup.currencyStock }}</el-descriptions-item>
-        <el-descriptions-item label="货币硬币转化率">{{ selectedGroup.currencyExchangeRate }}</el-descriptions-item>
-        <el-descriptions-item label="硬币储量">{{ selectedGroup.coinStock }}</el-descriptions-item>
-      </el-descriptions>
-
-      <el-text class="simple_text_red">群组评论</el-text>
-      <el-descriptions :column="2" border size="small">
-        <el-descriptions-item label="大热评">{{ selectedGroup.content[0] }}</el-descriptions-item>
-        <el-descriptions-item label="热评">{{ selectedGroup.content[1] }}</el-descriptions-item>
-        <el-descriptions-item label="评">{{ selectedGroup.content[2] }}</el-descriptions-item>
-      </el-descriptions>
-
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="detailsDialogVisible = false"
-                   class="clusterpop_close-button"
-        >关闭</el-button>
-      </span>
-
-    </el-dialog>
-
-
-    <!-- 踢出群组对话框 -->
-    <el-dialog
-        v-model="joinDialogVisible"
-        title="加入群组"
-    >
-
-      <p>确认将这家伙踢出群组 ?</p>
-
-
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="joinDialogVisible = false">取消</el-button>
-        <el-button type="primary" @click="confirmJoinGroup">确认</el-button>
-      </span>
-    </el-dialog>
 
     <!-- 用户信息弹框 -->
-    <el-dialog v-model="memberDialogVisible" title="用户详情">
+    <el-dialog
+        v-model="memberDialogVisible"
+        title="用户详情">
       <!-- 用户信息展示 -->
       <el-descriptions :column="2" border size="small">
         <el-descriptions-item label="账号">{{ selectedMember.account }}</el-descriptions-item>
@@ -182,6 +101,24 @@
       <!-- 操作按钮 -->
       <el-button type="danger" @click="kickOutMember(selectedMember.id)">踢出群聊</el-button>
     </el-dialog>
+
+
+    <!-- 踢出群组确认对话框 -->
+    <el-dialog
+        v-model="kickoutDialogVisible"
+        title="加入群组"
+    >
+
+      <p>确认将这家伙踢出群组 ?</p>
+
+
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="joinDialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="confirmKickout">确认</el-button>
+      </span>
+
+    </el-dialog>
+
 
   </div>
 </template>
@@ -244,19 +181,23 @@ const detailsDialogVisible = ref(false);
 const joinDialogVisible = ref(false);
 const memberDialogVisible = ref(false);
 
+const groupOptions = ref([]);
 
-// 查看详情
-const viewPopDetails = (group) => {
-  getOneCluster(group.id);
-  detailsDialogVisible.value = true;
+
+const selectedGroupId = ref(null);
+const selectedMember = ref({});
+
+
+const viewMemberDetails = (member) => {
+  selectedMember.value = member;
+  memberDialogVisible.value = true;
 };
 
-// 加入群组
-const joinGroup = (group) => {
-  Object.assign(selectedGroup, group);
-  joinDialogVisible.value = true;
-};
 
+// 群组容量百分比
+const groupCapacityPercentage = computed(() => {
+  return (selectedGroup.value.popVolume / selectedGroup.value.totalSpace) * 100;
+});
 
 //! 查
 
@@ -265,109 +206,6 @@ onMounted(async () => {
   await getAllClusterPage(pageData.current, pageData.size);
 });
 
-
-// 查询按钮点击事件
-const searchClusterCollect = () => {
-  getAllClusterPage(pageData.current, pageData.size);
-};
-
-
-/**
- * 分页获取群组数据
- */
-const getAllClusterPage = async (current, size) => {
-  try {
-    const {data} = await http({
-      url: http.adornUrl('Cluster/clusters/hall/all'),
-      method: 'get',
-      params: {
-        current,
-        size
-      }
-    });
-    pageData.current = data.current;
-    pageData.size = data.size;
-    pageData.total = data.total;
-    pageData.records = data.records.map(record => {
-      return {
-        ...record,
-        id: BigInt(record.id) // 将 id 转换为 BigInt 类型
-      };
-    });
-
-
-    if (pageData.total === 0) {
-      ElMessage({
-        message: '暂无数据',
-        type: 'warning',
-        duration: 1000
-      });
-    }
-
-  } catch (error) {
-    ElMessage({
-      message: '获取群组大厅数据失败: ' + error.message,
-      type: 'error',
-      duration: 1000
-    });
-  }
-
-};
-
-
-/**
- * id查单个群组详情
- */
-const getOneCluster = (id) => {
-  http({
-    url: http.adornUrl('Cluster/clusters/hall/one'),
-    method: 'get',
-    params: {
-      id
-      //id: id.toString() // 将 BigInt 转换为字符串传递给后端
-    }
-  }).then(({data}) => {
-    selectedGroup.value = data
-  }).catch((error) => {
-    ElMessage({
-      message: '获取群组详情失败: ' + error.message,
-      type: 'error',
-      duration: 1000
-    });
-  });
-}
-
-
-// 确认加入群组
-const confirmJoinGroup = async (clusterId) => {
-  try {
-    await http({
-      url: http.adornUrl('Cluster/clusters//hall/join'),
-      method: 'post',
-      params: {
-        clusterId: selectedGroup.id
-      }
-    });
-    ElMessage({
-      message: '加入群组成功',
-      type: 'success',
-      duration: 1000
-    });
-    joinDialogVisible.value = false;
-    await getAllClusterPage();
-  } catch (error) {
-    ElMessage({
-      message: '加入群组失败: ' + error.message,
-      type: 'error',
-      duration: 1000
-    });
-  }
-}
-
-// 搜索群组
-const groupOptions = ref([]);
-const selectedGroupId = ref(null);
-const selectedMember = ref({});
 
 const searchGroups = async (query) => {
   try {
@@ -386,14 +224,6 @@ const searchGroups = async (query) => {
   }
 };
 
-const groupCapacityPercentage = computed(() => {
-  return (selectedGroup.value.popVolume / selectedGroup.value.totalSpace) * 100;
-});
-
-const viewMemberDetails = (member) => {
-  selectedMember.value = member;
-  memberDialogVisible.value = true;
-};
 
 const kickOutMember = async (memberId) => {
   try {
@@ -408,7 +238,8 @@ const kickOutMember = async (memberId) => {
       duration: 1000
     });
     memberDialogVisible.value = false;
-    await getAllClusterPage(pageData.current, pageData.size);
+
+
   } catch (error) {
     ElMessage({
       message: '踢出群聊失败: ' + error.message,
