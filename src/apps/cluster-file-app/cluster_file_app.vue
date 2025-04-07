788bbe1702be8dc27943b4c4678f2ced772a799c
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2025-04-07 20:52:07 +0800

    群组文件* 1 突击技术攻坚, 基础原型设计 - 目标为基础按钮实现文件上传

--- src/apps/cluster-file-app/cluster_file_app.vue
+++ src/apps/cluster-file-app/cluster_file_app.vue
@@ -1,27 +1,27 @@
 <template>
 
 
-  <div class="level_app">
+  <div class="cluster_file_app">
 
     <!--应用标识-->
-    <el-text class="base_header">用户等级</el-text>
+    <el-text class="base_header">群组文件</el-text>
 
   </div>
 
   <!--左侧导航区域-->
-  <div class="level_app_left_nav">
+  <div class="cluster_file_app_left_nav">
 
     <el-menu default-active="1" class="el-menu-vertical" @select="handleMenuSelect">
       <!--一级菜单-->
       <el-menu-item index="1">
-        I. 用户等级
+        I. 文件管理
       </el-menu-item>
     </el-menu>
 
   </div>
 
   <!--具体页面-->
-  <component :is="currentView" class="level_app_compo" @close="backAppHome"/>
+  <component :is="currentView" class="cluster_file_app_compo" @close="backAppHome"/>
 
 </template>
 
@@ -105,7 +105,7 @@ const backAppHome = () => {
 <style lang="scss" scoped>
 
 
-.level_app {
+.cluster_file_app {
   display: flex;
   margin-top: -10px;
   flex-direction: row;
@@ -128,7 +128,7 @@ const backAppHome = () => {
 
 }
 
-.level_app_left_nav {
+.cluster_file_app_left_nav {
   width: 10%;
   margin-left: -150px;
   display: flex;
@@ -142,7 +142,7 @@ const backAppHome = () => {
 }
 
 //对应页面组件, 需要占满剩下的全部页面.
-.level_app_compo {
+.cluster_file_app_compo {
   flex: 1; /* 使 login_compo 占满剩余空间 */
   display: flex;
   flex-direction: column;
